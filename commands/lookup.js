d5265fea01945d96859cd156ff05dcc2a2ffe534
CJ <cjtheham@protonmail.com>
2022-04-11 13:27:14 -0500

    lookups no longer pings hamcall for invalid calls

--- commands/lookup.js
+++ commands/lookup.js
@@ -2,10 +2,12 @@ const { default: axios } = require("axios");
 const { MessageEmbed } = require("discord.js");
 
 let address, last_lotw;
+const regex = new RegExp(/^[AaWaKkNn][a-zA-Z]?[0-9][a-zA-Z]{1,3}$/);
 
 module.exports.execute = async (client, message, args) => {
     if (args.length) {
-        axios.get(`https://hamcall.dev/${args[0]}.json`)
+        if (regex.test(args[0])) {
+            axios.get(`https://hamcall.dev/${args[0]}.json`)
             .then(function (response) {
                 if (response.data.address == '') {
                     address = `P.O. BOX ${response.data.po_box}` 
@@ -29,8 +31,11 @@ module.exports.execute = async (client, message, args) => {
                 return message.channel.send({ embeds: [ embed ] })
             })
             .catch(function (error) {
-                return message.channel.send('no bueno por favor')
+                return message.channel.send('Cannot find call on hamcall.dev.')
             })
+        } else {
+            return message.channel.send('This callsign is invalid.')
+        }
     } else {
         return await message.channel.send('Please give me a callsign to search for.')
     }
